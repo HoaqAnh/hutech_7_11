aef5a0585e66b741f9ffa21a9205598bdb4516b8
Hoang Anh <288hoanganh@gmail.com>
2024-02-29 17:13:47 +0700

    first cm

---
+++ Animals.java
@@ -0,0 +1,39 @@
+public class Animals {
+    private String eat;
+    private String name;
+    public String getEat(){
+        return eat;
+    }
+    public void setEat(String eat){
+        this.eat = eat ;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
+class contho extends Animals{
+    private int anCorrot;
+    public int getAnCorrot(){ return anCorrot; }
+    public void setAnCorrot(int anCorrot){
+        this.anCorrot = anCorrot;
+    }
+}
+class conho extends Animals{
+    private String anThit ;
+    public String getAnThit(){
+        return anThit;
+    }
+    public void setAnThit(String anThit){ this.anThit = anThit; }
+}
+class conbo extends Animals{
+    private String anCo;
+    public String getAnCo(){
+        return anCo;
+    }
+    public void setAnCo(String anCo){
+        this.anCo = anCo;
+    }
+}
